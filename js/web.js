5e95f860760a8bff6d66fd2a362b9c65008031ba
Hector <hectoruch1@hotmail.com>
2015-09-27 16:48:14 +0200

    ..
    .

---
+++ js/web.js
@@ -0,0 +1,16 @@
+var texto1 ="See Soon!";
+var texto2 ="Au revoir";
+var texto3 ="That new girl";
+var texto4 ="Pinky nose";
+var texto5 ="Sweet mouth";
+var texto6 ="Beauty cat";
+var texto7 ="Unreal life";
+var texto8 ="Summer like Spring";
+
+var fuente1 = "";
+var fuente2 = "";
+var fuente3 = "";
+var fuente4 = "";
+var fuente5 = "";
+var fuente6 = "";
+var fuente7 = "";
