79189ac70a38edf8b683ab5686ef8e96e78f753a
Hector <hectoruch1@hotmail.com>
2015-09-27 16:54:01 +0200

    .
    .

--- js/web.js
+++ js/web.js
@@ -7,10 +7,17 @@ var texto6 ="Beauty cat";
 var texto7 ="Unreal life";
 var texto8 ="Summer like Spring";
 
-var fuente1 = "";
-var fuente2 = "";
-var fuente3 = "";
-var fuente4 = "";
+var fuente1 = "CabinSketch-Regular.ttf";
+var fuente2 = "Damion-Regular.ttf";
+var fuente3 = "MarkoOne-Regular.ttf";
+var fuente4 = "McLaren-Regular.ttf";
 var fuente5 = "";
 var fuente6 = "";
 var fuente7 = "";
+
+var rtext = Math.floor((Math.random() * 8) + 1);
+var rfuent = Math.floor((Math.random() * 4) + 1);
+
+function loadfontprimary(){
+	
+}
