01520e9b09a01c1e1be720f8c18fea7784503c0d
Hector <hectoruch1@hotmail.com>
2015-11-16 09:21:24 +0100

    .
    .

--- js/web.js
+++ js/web.js
@@ -2,8 +2,14 @@ var link1 = "https://www.google.com/fonts#QuickUsePlace:quickUse/Family:Cabin+Sk
 var link2 = "https://www.google.com/fonts#QuickUsePlace:quickUse/Family:Damion";
 var link3 = "https://www.google.com/fonts#QuickUsePlace:quickUse/Family:Marko+One";
 var link4 = "https://www.google.com/fonts#QuickUsePlace:quickUse/Family:McLaren";
+var link5 = "https://www.google.com/fonts#QuickUsePlace:quickUse/Family:Amatic+SC";
+var link6 = "https://www.google.com/fonts#QuickUsePlace:quickUse/Family:Architects+Daughter";
+var link7 = "https://www.google.com/fonts#QuickUsePlace:quickUse/Family:Bangers";
+var link8 = "https://www.google.com/fonts#QuickUsePlace:quickUse/Family:Black+Ops+One";
+var link9 = "https://www.google.com/fonts#QuickUsePlace:quickUse/Family:Coming+Soon";
+var link10 = "https://www.google.com/fonts#QuickUsePlace:quickUse/Family:Covered+By+Your+Grace";
 
-var a=[link1,link2,link3,link4];
+var a=[link1,link2,link3,link4,link5,link6,link7,link8,link9,link10];
 
 $(window).resize(function () {
 	$("header").css('height', $(window).height() + "px");
@@ -21,7 +27,7 @@ function createfonts() {
 	$("#herefonts").css("display","block");
 	$("#herefonts").html("");
 	$('html, body').animate({scrollTop:$("#herefonts").offset().top},1000);
-	for (var i = 0; i < 4; i++) {
+	for (var i = 0; i < 10; i++) {
 		var valor = Math.floor((Math.random() * 3) + 2);
 		$("#herefonts").append("<a href='"+a[i]+"' target='_blank'><div class='kol-"+valor+" textmedium' style='font-family:fuente"+i+"'><div class='kol-90 centerkol whereisthefont'>"+$("#textuser").val()+"</div></div></a>")
 	}
