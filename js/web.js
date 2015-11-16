9f2152895dd9b6d9c87772ad1630dba8d1854f87
Hector <hectoruch1@hotmail.com>
2015-11-16 08:54:40 +0100

    .
    .

--- js/web.js
+++ js/web.js
@@ -23,7 +23,7 @@ function createfonts() {
 	$('html, body').animate({scrollTop:$("#herefonts").offset().top},1000);
 	for (var i = 1; i <= 4; i++) {
 		var valor = Math.floor((Math.random() * 3) + 2);
-		$("#herefonts").append("<a href='"+link+""+i+"' targe='_blank'><div class='kol-"+valor+" textmedium' style='font-family:fuente"+i+"'><div class='kol-90 centerkol whereisthefont'>"+$("#textuser").val()+"</div></div></a>")
+		$("#herefonts").append("<a href='"+link+i+"' targe='_blank'><div class='kol-"+valor+" textmedium' style='font-family:fuente"+i+"'><div class='kol-90 centerkol whereisthefont'>"+$("#textuser").val()+"</div></div></a>")
 	}
 }
 
