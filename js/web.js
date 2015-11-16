e2514b618e73cfdc8b063695447500a7c3dbb060
Hector <hectoruch1@hotmail.com>
2015-11-16 08:50:06 +0100

    .
    .

--- js/web.js
+++ js/web.js
@@ -1,3 +1,10 @@
+var link1 = "https://www.google.com/fonts#QuickUsePlace:quickUse/Family:Cabin+Sketch";
+var link2 = "https://www.google.com/fonts#QuickUsePlace:quickUse/Family:Damion";
+var link3 = "https://www.google.com/fonts#QuickUsePlace:quickUse/Family:Marko+One";
+var link4 = "https://www.google.com/fonts#QuickUsePlace:quickUse/Family:McLaren";
+
+
+
 $(window).resize(function () {
 	$("header").css('height', $(window).height() + "px");
 	$("#herefonts").css('height', $(window).height() + "px");
@@ -16,7 +23,7 @@ function createfonts() {
 	$('html, body').animate({scrollTop:$("#herefonts").offset().top},1000);
 	for (var i = 1; i <= 4; i++) {
 		var valor = Math.floor((Math.random() * 3) + 2);
-		$("#herefonts").append("<div class='kol-"+valor+" textmedium' style='font-family:fuente"+i+"'><div class='kol-90 centerkol whereisthefont'>"+$("#textuser").val()+"</div></div>")
+		$("#herefonts").append("<a href='link"+i+"' targe='_blank'><div class='kol-"+valor+" textmedium' style='font-family:fuente"+i+"'><div class='kol-90 centerkol whereisthefont'>"+$("#textuser").val()+"</div></div></a>")
 	}
 }
 
